2fe5937c2f7dd3c71e3de59675572f8fad6884d5
Wendell Sun <iwendellsun@gmail.com>
2016-07-19 19:29:59 +0800

    Add drone config file

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-<<<<<<< HEAD
 import (
 	"github.com/kataras/iris"
 )
@@ -10,15 +9,5 @@ func main() {
 	iris.Get("/hi", func(ctx *iris.Context) {
 		ctx.Write("Hi %s", "iris")
 	})
-	iris.Listen(":8080")
-=======
-import "github.com/kataras/iris"
-
-func main() {
-	iris.Get("/test", func(ctx *iris.Context) {
-		ctx.Write("It's OK!")
-	})
-
-	iris.Listen(":6000")
->>>>>>> a5db72ba10cc61fc48411b451e8a281991891212
+	iris.Listen(":5000")
 }
