ed21f1725722552b3d44c7ebe11d933ed89ea7b1
mojo <excessivespeed@126.com>
2016-07-26 23:35:48 +0800

    add template||html

--- model/user.go
+++ model/user.go
@@ -9,12 +9,12 @@ import (
 
 type User struct {
 	Id         bson.ObjectId `json:"id,omitempty"`
-	UserName   string        `json:"userName,omitempty"`   //姓名
-	Gender     string        `json:"Gender,omitempty"`     //性别
-	NickName   string        `json:"nickName,omitempty"`   //外号
-	PinYin     string        `json:"pinyin,omitempty"`     //简拼
-	FullPinYin string        `json:"fullPinYin,omitempty"` //全拼
-	PassWord   string        `json:"passWord,omitempty"`
+	UserName   string        `json:"userName,omitempty" form:"userName"` //姓名
+	Gender     string        `json:"Gender,omitempty"`                   //性别
+	NickName   string        `json:"nickName,omitempty"`                 //外号
+	PinYin     string        `json:"pinyin,omitempty"`                   //简拼
+	FullPinYin string        `json:"fullPinYin,omitempty"`               //全拼
+	PassWord   string        `json:"passWord,omitempty" form:"passWord"`
 	Salt_Hash  string        `json:"-"`
 	Salt       string        `json:"-"`
 }
--- routers/routers.go
+++ routers/routers.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"github.com/flourish-ship/work-attendance/api"
+	"github.com/flourish-ship/work-attendance/render"
 	"github.com/flourish-ship/work-attendance/socket"
 	"github.com/kataras/iris"
 )
@@ -11,8 +12,10 @@ func Routers() (frame *iris.Framework) {
 	frame = iris.New()
 	userApi := api.UserApi{}
 	chatApi := socket.ChartRoomApi{}
+	homeRender := render.HomeRender{}
 
 	userApi.Register(frame)
+	homeRender.Register(frame)
 	chatApi.Chart(frame)
 
 	return
